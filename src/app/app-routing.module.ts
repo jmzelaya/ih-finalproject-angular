d28f5ee20d9c75e49626aed2687bd9a9350a81ff
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-08 14:59:49 -0400

    added bootstrap styles and layouts to sign up and userhome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
+import { UserHomePageComponent } from './pages/user-home-page/user-home-page.component';
+
 
 
 const routes: Routes = [
   {path: "", component: SignUpComponent},
   {path: "login", component: LogInComponent},
+  {path: "user-home", component: UserHomePageComponent},
   {path: '**', component: SignUpComponent}
 ];
 
