8d486d03670971a8bb9394c266d49bda94ac6a2e
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-10 10:33:27 -0400

    now displaying all supplies

--- src/app/pages/supply-page/supply-page.component.ts
+++ src/app/pages/supply-page/supply-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SupplyApiService } from '../../services/supply-api.service';
 import { HttpClient } from '@angular/common/http';
+import { SupplyInfo } from '../../interfaces/supply-info';
 
 @Component({
   selector: 'app-supply-page',
@@ -11,12 +12,28 @@ export class SupplyPageComponent implements OnInit {
 
   baseUrl: string = 'http://localhost:3000'
 
+  supplies: any[] = [];
+
+  newSupply: SupplyInfo = {
+    productName: '',
+    productDescription: '',
+    productValue: '',
+    tag: ''
+  }
+
   constructor(
     private supplyThang: SupplyApiService,
     private httpThang: HttpClient
   ) { }
 
   ngOnInit() {
-  }
+    this.supplyThang.getSupplies()
+      .subscribe(
+        (suppliesFromApi: any[]) => {
+          this.supplies = suppliesFromApi;
+        }
+      );//CLOSE this.postThang.getMyPosts()
+    }
 
-}
+
+}//CLOSE class SupplyPageComponent
